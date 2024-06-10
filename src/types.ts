0b414703671536bf9e1f793d9fa667408c91e0bd
Simon Petrac <spetrac@nicos-ag.com>
2024-06-10 15:20:23 +0200

    wip: added a FileManager class

--- src/types.ts
+++ src/types.ts
@@ -3,24 +3,12 @@ import type { DataFactory, Stream, Quad, NamedNode } from '@rdfjs/types'
 
 export type * from '@rdfjs/types'
 
-/** 
- * The DOMString type corresponds to the set of all possible sequences 
- * of 16 bit unsigned integer code units to be interpreted as UTF-16 encoded strings. 
- */
-export type DOMString = string
-
 export interface ConstructorOptions {
   /** DataFactory implementation that will be used to create all the Data Model instances. */
   dataFactory?: DataFactory
   /** Base IRI that will be used to resolve or create relative IRIs. */
   baseIRI?: DOMString
 }
-
-/** 
- * The PathString type corresponds to file paths on the respective system 
- * that this node instance is running on.
- */
-export type PathString = string
 
 export type ResultEvents = {
   end: []
@@ -42,6 +30,10 @@ export type QuadStream = EventEmitter<StreamEvents> & Stream<Quad>
 export type Awaitable<T> = T | Promise<T>
 export type TypeMap<Types = Record<Token, any>> = Record<Token & keyof Types, any>
 
+export function isNull(value: unknown): value is null | undefined {
+  return (value ?? null) === null
+}
+
 export function isString(value: unknown): value is string {
   return typeof value === 'string'
 }
@@ -54,15 +46,41 @@ export function isFunction(value: unknown): value is Function {
   return typeof value === 'function'
 }
 
+export function isObject(value: unknown): value is Record<any, any> {
+  return !!value && typeof value === 'object'
+}
+
+/** 
+ * The DOMString type corresponds to the set of all possible sequences 
+ * of 16 bit unsigned integer code units to be interpreted as UTF-16 encoded strings. 
+ */
+export type DOMString = string
+
+/** 
+ * The PathString type corresponds to file paths on the respective system 
+ * that this node instance is running on.
+ */
+export type PathString = string
+
+export function isPathString(value: unknown): value is PathString {
+  return isString(value)
+}
+
 export type Token = string
-export type Byte = number
-export type Position = number
-export type ByteSize = number
 
 export function isToken(value: unknown): value is Token {
   return isString(value)
     && /^\S+$/.test(value)
 }
+
+export type FileEnding = `.${string}`
+
+export function isFileEnding(value: unknown): value is FileEnding {
+  return isString(value)
+    && /^\.\w+$/.test(value)
+}
+
+export type Byte = number
 
 export function isByte(value: unknown): value is Byte {
   return isNumber(value)
@@ -71,12 +89,16 @@ export function isByte(value: unknown): value is Byte {
     && value <= 0xff
 }
 
+export type Position = number
+
 export function isPosition(value: unknown): value is Position {
   return isNumber(value)
     && Number.isInteger(value)
     && value >= 0x00000000
     && value <= 0xffffffff
 }
+
+export type ByteSize = number
 
 export function isByteSize(value: unknown): value is ByteSize {
   return isNumber(value)
@@ -85,6 +107,8 @@ export function isByteSize(value: unknown): value is ByteSize {
     && value <= 0xffffffff
 }
 
-export function isData<T = unknown>(value: T): value is T extends undefined ? never : T {
+export type Data = Object | null
+
+export function isData(value: unknown): value is Data {
   return value !== undefined
 }
